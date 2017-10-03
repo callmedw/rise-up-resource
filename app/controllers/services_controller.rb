5c634ac037e3937913cccb97526beaf000f33cdb
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-10-03 15:50:21 -0700

    add @services to services controller, add images and add logo html

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,2 +1,13 @@
 class ServicesController < ApplicationController
+  before_action :admin_authorize, only: [:new, :create, :edit, :update, :destroy]
+
+  def current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+  end
+
+  def index
+    @services = Service.all
+  end
 end
